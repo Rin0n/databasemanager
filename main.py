76ea7c1b8eb229d0adbf4ef30826fc4044b82405
Rin0n <wnatthis1234@gmail.com>
2025-12-27 22:30:57 +0500

    Remove SQLite database connection code

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-import sqlite3
-conn = sqlite3.connect('movie.db')
-cur = conn.cursor()
-cur.execute('''SELECT * FROM movies ORDER BY popularity DESC LIMIT 1''')
-#cur.execute('''SELECT * FROM movies WHERE release_date BETWEEN '2009-12-01' AND '2009-12-31' ORDER BY budget DESC LIMIT 1 ''')
+
